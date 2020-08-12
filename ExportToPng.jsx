311220bf4dce8f7ac2a26d5ca0f789207829e939
sshuyii <43859049+sshuyii@users.noreply.github.com>
2020-08-12 01:00:41 -0400

    only visible layers can be exported

--- ExportToPng.jsx
+++ ExportToPng.jsx
@@ -1,12 +1,10 @@
-//based on work from https://gist.github.com/tomekc/2892034
-
 //Set Photoshop to use pixel measurements  
 app.preferences.rulerUnits = Units.PIXELS  
 
 var docRef = app.activeDocument;  
 var newDocRef;
 
-var newPath = "your path";
+var newPath = "yourpath";//need to set a specific path
     
 scanLayerSets(docRef);
 
@@ -31,6 +29,8 @@ function duplicateGroupToDoc(layer)
     if (x > 0)
     {     
         app.activeDocument = docRef;//set active document
+        activeDocument.activeLayer = layer;
+
         newLayerSetRef = layer.duplicate(app.documents["Clipped Group"], ElementPlacement.PLACEATBEGINNING);  
         app.activeDocument = newDocRef;             
     }
@@ -43,15 +43,18 @@ function scanLayerSets(scanFile)
     // find layer groups
     for(var i = 0; i < scanFile.layerSets.length; i++){
         var lsName = scanFile.layerSets[i].name;
-        if (lsName.substr(-4) == ".png") //这个layerSet为png结尾
-        {
-            duplicateGroupToDoc(scanFile.layerSets[i]);//create a new doc including only one layer group
+        if(scanFile.layerSets[i].visible == true)
+        {    
+           if (lsName.substr(-4) == ".png") 
+            {
+                duplicateGroupToDoc(scanFile.layerSets[i]);//create a new doc including only one layer group
             
-            newDocRef.mergeVisibleLayers(); 
+                newDocRef.mergeVisibleLayers(); 
 
-            saveLayer(newDocRef.layers.getByName(lsName), lsName, newPath);
+                saveLayer(newDocRef.layers.getByName(lsName), lsName, newPath);
+            }
+            scanLayerSets(scanFile.layerSets[i]);
         }
-        scanLayerSets(scanFile.layerSets[i]);
         //  else {
         //     //recursive
         //     //works for .png group nests within non-png group
@@ -64,8 +67,8 @@ function scanLayerSets(scanFile)
     // find plain layers in current group whose names end with .png
     for(var j = 0; j < scanFile.artLayers.length; j++) {
         var name = scanFile.artLayers[j].name;
-        if (name.substr(-4) == ".png") {
-            duplicateGroupToDoc(scanFile.artLayers[j]);/
+        if (name.substr(-4) == ".png" && scanFile.artLayers[j].visible == true) {
+            duplicateGroupToDoc(scanFile.artLayers[j]);
             saveLayer(newDocRef.layers.getByName(name), name, newPath);
         }
     }
